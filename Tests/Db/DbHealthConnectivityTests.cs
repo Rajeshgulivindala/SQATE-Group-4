10039edcc2d7d3badf83bd552530b15584ddff8e
Robinjit kaur <robinjitkaur7197@conestogac.on.ca>
2025-10-09 20:02:44 -0400

    Added Integration test: DB connectivity + trivial query

---
+++ Tests/Db/DbHealthConnectivityTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HospitalManagementSystem.Tests.Infrastructure; // BaseIntegrationTest with Cs
+
+namespace HospitalManagementSystem.Tests.Db
+{
+    [TestClass, TestCategory("Integration-DB")]
+    public class DbHealthConnectivityTests : BaseIntegrationTest
+    {
+        [TestMethod]
+        public void Can_Open_Connection_And_Run_Scalar()
+        {
+            using (var con = new SqlConnection(Cs))
+            {
+                con.Open();
+                using (var cmd = new SqlCommand("SELECT CAST(1 AS INT)", con))
+                {
+                    var val = (int)cmd.ExecuteScalar();
+                    Assert.AreEqual(1, val);
+                }
+            }
+        }
+    }
+}
